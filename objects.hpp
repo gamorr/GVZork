8548da085c494a912b706b5a221e10775a56c394
Harrywaldon <156609909+Harrywaldon@users.noreply.github.com>
2024-11-15 13:13:39 -0500

    Adds npc class to objects.h

--- objects.hpp
+++ objects.hpp
@@ -26,6 +26,26 @@ class Item {
         float weight;
 };
 
+class NPC {
+    public:
+        NPC(const std::string& name, const std::string& description);
+        void set_name(const std::string& n);
+        void set_description(const std::string& desc);
+        void add_message(const std::string& msg);
+        std::string get_name() const;
+        std::string get_description() const;
+        //returns the current message according to the message number. Increments the message number
+        std::string get_message();
+
+    private:
+        std::string name;
+        std::string description;
+        //vector of messages that an NPC has. The current message updates whenever the player talks to the NPC
+        std::vector<std::string> messages;
+        //index of the current message
+        int message_number;
+};
+
 class Location {
     public:
         // Constructor
